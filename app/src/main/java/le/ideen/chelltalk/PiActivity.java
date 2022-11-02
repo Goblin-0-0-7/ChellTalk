11bfe75a3ea76a1ec4af09e27fc1457bf4ce78b7
Goblin <mc.goblin@t-online.de>
2022-11-03 00:27:56 +0100

    fixed sliders and added editable Text to tune sliders

--- app/src/main/java/le/ideen/chelltalk/PiActivity.java
+++ app/src/main/java/le/ideen/chelltalk/PiActivity.java
@@ -1,6 +1,7 @@
 package le.ideen.chelltalk;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Handler;
 import android.os.Message;
@@ -8,8 +9,6 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.UUID;
 
 import android.bluetooth.BluetoothAdapter;
@@ -17,16 +16,23 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class PiActivity extends AppCompatActivity {
 
     SeekBar sb_red, sb_green, sb_blue;
+    EditText et_red_value, et_green_value, et_blue_value;
     private int[] rgb_value = {0,0,0};
     public BluetoothAdapter mmBTAdapter = BluetoothAdapter.getDefaultAdapter();
     private static final String TAG = "PiActivity";
@@ -113,7 +119,7 @@ public class PiActivity extends AppCompatActivity {
         String red_value = Integer.toString(rgb_value[0]);
         String green_value = Integer.toString(rgb_value[1]);
         String blue_value = Integer.toString(rgb_value[2]);
-        String msg = red_value + "," + green_value + "," + blue_value;
+        String msg = red_value + "," + green_value + "," + blue_value + ","; //the "," at the end is for seperation if two msg get sent together
         try{
             BTThread.write(msg.getBytes());
         } catch (IOException e) {
@@ -129,15 +135,91 @@ public class PiActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pi);
 
+        et_red_value = (EditText) findViewById(R.id.editTextNumber_red_value);
+        et_green_value = (EditText) findViewById(R.id.editTextNumber_green_value);
+        et_blue_value = (EditText) findViewById(R.id.editTextNumber_blue_value);
+
+        //editText Listeners on Done
+        et_red_value.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        et_red_value.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    try {
+                        if (Integer.parseInt(et_red_value.getText().toString()) > 255) {
+                            sb_red.setProgress(255);
+                        } else {
+                            sb_red.setProgress(Integer.parseInt(et_red_value.getText().toString()));
+                        }
+                    }
+                    catch (NumberFormatException e){
+                        sb_red.setProgress(0);
+                    }
+                    InputMethodManager imm = (InputMethodManager)v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        et_green_value.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        et_green_value.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    try {
+                        if (Integer.parseInt(et_green_value.getText().toString()) > 255) {
+                            sb_green.setProgress(255);
+                        } else {
+                            sb_green.setProgress(Integer.parseInt(et_green_value.getText().toString()));
+                        }
+                    }
+                    catch (NumberFormatException e){
+                        sb_green.setProgress(0);
+                    }
+                    InputMethodManager imm = (InputMethodManager)v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        et_blue_value.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        et_blue_value.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    try {
+                        if (Integer.parseInt(et_blue_value.getText().toString()) > 255) {
+                            sb_blue.setProgress(255);
+                        } else {
+                            sb_blue.setProgress(Integer.parseInt(et_blue_value.getText().toString()));
+                        }
+                    }
+                    catch (NumberFormatException e){
+                        sb_blue.setProgress(0);
+                    }
+                    InputMethodManager imm = (InputMethodManager)v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                    return true;
+                }
+                return false;
+            }
+        });
+
         sb_red = (SeekBar) findViewById(R.id.seekBar_red);
         sb_green = (SeekBar) findViewById(R.id.seekBar_green);
         sb_blue = (SeekBar) findViewById(R.id.seekBar_blue);
+        //seekBar listeners
         sb_red.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
 
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress,
                                           boolean fromUser) {
                 rgb_value[0] = sb_red.getProgress();
+                et_red_value.setText(Integer.toString(rgb_value[0]));
                 sendColorCode();
             }
 
@@ -157,6 +239,7 @@ public class PiActivity extends AppCompatActivity {
             public void onProgressChanged(SeekBar seekBar, int progress,
                                           boolean fromUser) {
                 rgb_value[1] = sb_green.getProgress();
+                et_green_value.setText(Integer.toString(rgb_value[1]));
                 sendColorCode();
             }
 
@@ -176,6 +259,7 @@ public class PiActivity extends AppCompatActivity {
             public void onProgressChanged(SeekBar seekBar, int progress,
                                           boolean fromUser) {
                 rgb_value[2] = sb_blue.getProgress();
+                et_blue_value.setText(Integer.toString(rgb_value[2]));
                 sendColorCode();
             }
 
